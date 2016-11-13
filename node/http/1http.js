5cffb1a2d526f7220270fa4c6a1aedad3b7aa5a1
xiaoxiao <xiaoxia0@51huxin.com>
2016-11-13 12:14:18 +0800

    js

--- node/http/1http.js
+++ node/http/1http.js
@@ -3,23 +3,31 @@
  */
 
 var  http= require('http');
+var fs =require('fs');
 
 function serve(request,response) {
 	//headers
 	//method
 	//url
 
-	console.log(request);
+	console.log(request.url);
 
 	//相应
-	response.statusCode=404
+	//response.statusCode = 404;
+	response.statusCode = 200;
 	response.setHeader('name',"zfpx");
-	//response.setHeader('Content-Type',"text/html;charest=utf-8")
-	response.write(new Date().toString());
+	response.setHeader('Content-Type',"text/html;charest=utf-8")
+	fs.readFile('./index.html',function (err,data) {
+		if(err){
+		}
+		response.write(data);
+		response.end();
+	})
+	//response.write(new Date().toString());
 
 
 
-	response.end()
+	//response.end()
 }
 // 每当请求来的时候处理
 var server = http.createServer(serve);
